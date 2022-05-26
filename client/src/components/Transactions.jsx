afb9bc130971eaff4ba2e5630cf568de14f1e3aa
daenamcclintock <daena.mcclintock@gmail.com>
2022-05-26 16:04:27 -0400

    logo and footer edits

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -3,7 +3,6 @@ import React, { useContext } from "react";
 import { TransactionContext } from "../context/TransactionContext";
 
 import useFetch from "../hooks/useFetch";
-import dummyData from "../utils/dummyData";
 import { shortenAddress } from "../utils/shortenAddress";
 
 const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword, amount, url }) => {
@@ -64,7 +63,7 @@ const Transactions = () => {
         )}
 
         <div className="flex flex-wrap justify-center items-center mt-10">
-          {[...dummyData, ...transactions].reverse().map((transaction, i) => (
+          {[...transactions].reverse().map((transaction, i) => (
             <TransactionsCard key={i} {...transaction} />
           ))}
         </div>
